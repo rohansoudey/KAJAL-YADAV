503936e490846c83b8efa325108372ce8dc5d286
Rohan Soudey <34374980+rohansoudey@users.noreply.github.com>
2019-01-22 15:51:45 +0530

    Add files via upload

---
+++ Game.py
@@ -0,0 +1,17 @@
+arr=[1,2,3]
+arr1=[4,5,6]
+arr2=[7,8,9]
+#for i in arr:
+print(arr)
+print(arr1)
+print(arr2)
+pos1=input("Player 1 Enter Position of your choice\n")
+#print("you enter",pos1)
+if(pos1=='1'):
+    arr[0] = str('X')
+    print(arr)
+    print(arr1)
+    print(arr2)
+
+    
+
